c9c50ce87559f88fa18420a04bd2a87d50a6a017
Mobilewits Mobile apps developers <harry@Harrys-Mac-Pro.local>
2015-03-26 15:45:03 -0400

    How to set Simple Application Icon Xamarin.iOS

---
+++ SampleApplicationIcon.iOSViewController.designer.cs
@@ -0,0 +1,17 @@
+﻿//
+// This file has been generated automatically by MonoDevelop to store outlets and
+// actions made in the Xcode designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+
+namespace SampleApplicationIcon.iOS
+{
+	[Register ("SampleApplicationIcon_iOSViewController")]
+	partial class SampleApplicationIcon_iOSViewController
+	{
+		void ReleaseDesignerOutlets ()
+		{
+		}
+	}
+}
